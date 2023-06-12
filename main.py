aa44b76ff87304ea3aed7df62dd769892afb6183
Aufa Fauqi Ardhiqi <ardhiqi@gmail.com>
2023-06-12 21:37:55 +0700

    add api_key

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from flask import Flask, request,jsonify,make_response
 from datetime import datetime, timedelta
 from prediction import model_predict
 from functools import wraps
-import os
-import jwt
 from werkzeug.utils import secure_filename
-import time
 import firebase_admin
 from firebase_admin import credentials, storage
 
@@ -14,9 +11,22 @@ firebase_admin.initialize_app(cred,{
     'storageBucket': 'capstone-fruit-fresh-detection.appspot.com'
 })
 
+API_KEY = "QzIzLVBTNTk0LUJpc21pbGxhaC1MdWx1cw=="
 app = Flask(__name__)
+def api_key_required(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = request.args.get('API_KEY')
+        if not token:
+            return jsonify({'Message': 'API KEY is missing!'}), 401
+        if token != API_KEY:
+            return jsonify({"Message":'Invalid key!'}),403
+        return func(*args, **kwargs)
+    return decorated
+
 
 @app.route('/',methods=['GET','POST'])
+@api_key_required
 def index():
     if request.method == "POST":
         file = request.files.get('file')
@@ -36,6 +46,7 @@ def index():
 
 
 @app.route('/upload',methods=['POST'])
+@api_key_required
 def upload():
     if request.method == "POST":
         file = request.files.get('file')
@@ -54,6 +65,7 @@ def upload():
     return "OK"
 
 @app.route('/predict',methods=['POST'])
+@api_key_required
 def predict():
     url = request.get_json()['url']
     if url is None or url == "":
@@ -65,6 +77,7 @@ def predict():
             return jsonify({"error": str(e)})
 
 @app.route('/upload-predict',methods=['POST'])
+@api_key_required
 def uploadThenPredict():
     if request.method == "POST":
         file = request.files.get('file')
